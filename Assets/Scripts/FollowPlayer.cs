034d952ef04bbe5a4bb27a6890b7f09d3a1594d6
JohnnyTendo <JohnnyTendo@github.com>
2019-02-22 19:14:24 +0100

    Initial commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public Quaternion rotationOffset;
+    Transform cameraTransform;
+
+    void Start()
+    {
+        cameraTransform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        cameraTransform.position = target.position + offset;
+        cameraTransform.rotation = rotationOffset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    public float jumpSpeed;
+    public float moveSpeed;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
+        }
+    }
+}
